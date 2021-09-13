fec55c2d0ad71678614ab5d66946d7b0e06aeac4
rksdev <mailto.dev.rakesh@gmail.com>
2021-09-14 00:15:28 +0530

    Added functionality: Enter, Shift, Capslock, toggle: numbers and Symbols, fixed layout

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let input = "";
 let deleteContent = "";
+let capslockCounter = 0;
+
 function toTextArea(inp) {
-  input += inp.innerText;
+  if (capslockCounter == 0 || capslockCounter % 2 == 0) {
+    if (inp.innerHTML[4] == "s") {
+      input += inp.innerText.slice(inp.innerText.length - 1);
+    } else {
+      input += inp.innerText.toLowerCase();
+    }
+  } else {
+    if (inp.innerHTML[4] == "s") {
+      input += inp.innerText.slice(0, 1);
+    } else {
+      input += inp.innerText;
+    }
+  }
   document.getElementById("text").value = input;
 }
 
@@ -16,3 +30,13 @@ function backspace() {
 //     input = input.substr(0, input.length - 1);
 //     document.getElementById("text").value = input;
 //   }
+
+function capslock() {
+  capslockCounter++;
+}
+
+function enter() {
+  let ent = "\r\n";
+
+  input += ent;
+}
